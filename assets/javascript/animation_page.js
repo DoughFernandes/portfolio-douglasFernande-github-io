1134214bdad920343ce96803ece5e246e3953683
Douglas Fernandes <dfdsf1996@gmail.com>
2024-04-05 15:32:09 -0300

    Finalização do portfolio

--- assets/javascript/animation_page.js
+++ assets/javascript/animation_page.js
@@ -1,19 +1,13 @@
-// Inicializa o ScrollReveal
 const sr = ScrollReveal({
-    origin: 'left', // De onde os elementos vão aparecer
-    distance: '50px', // A distância que os elementos vão percorrer
-    duration: 2000, // Duração da animação em milissegundos
-    reset: true // Define se a animação deve ocorrer toda vez que o elemento aparecer na tela
+    origin: 'left', 
+    distance: '1000px', 
+    duration: 2000,
+    reset: true
   });
   
-
-
-// Seleciona os elementos e aplica a animação
 document.addEventListener('DOMContentLoaded', function () {
     sr.reveal('#animation__display', { 
-        delay: 0, // Atraso antes da animação começar
-        viewFactor: 0.5 // A porcentagem do elemento que precisa estar visível para iniciar a animação
+        delay: 0,
+        reset: false
     });
 });
-
-  
