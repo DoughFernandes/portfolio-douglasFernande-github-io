c0d545db737bcb585700711c552720ef5ffa686a
Douglas Fernandes <dfdsf1996@gmail.com>
2024-04-09 16:38:00 -0300

    Thema JS upadade

--- assets/javascript/theme.js
+++ assets/javascript/theme.js
@@ -1,17 +1,26 @@
 const iconTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement;
 
-
 iconTheme.addEventListener('click', () => {
     const currentTheme = rootHtml.getAttribute('data-theme');
     const classType = iconTheme.className;
 
-    if(classType === 'fa-regular fa-sun'){
+    if(classType === 'fa-regular fa-sun') {
         iconTheme.className = 'fa-solid fa-cloud-moon';
-        rootHtml.setAttribute('data-theme',"light");
+        rootHtml.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); // Salva o tema 'light'
 
-    }if(classType === 'fa-solid fa-cloud-moon'){
-        iconTheme.className = 'fa-regular fa-sun'
-        rootHtml.setAttribute('data-theme',"dark");
+    } else if(classType === 'fa-solid fa-cloud-moon') {
+        iconTheme.className = 'fa-regular fa-sun';
+        rootHtml.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); // Salva o tema 'dark'
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if(savedTheme) {
+        rootHtml.setAttribute('data-theme', savedTheme);
+        iconTheme.className = savedTheme === 'dark' ? 'fa-regular fa-sun' : 'fa-solid fa-cloud-moon';
     }
 });
