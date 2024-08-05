867190646e36cf8b99280ae1eae57b859c3415d4
Douglas Fernandes <dfdsf1996@gmail.com>
2024-08-05 11:18:24 -0300

    Página contatos feita com exito

--- src/app/pages/contact/desktop/index.tsx
+++ src/app/pages/contact/desktop/index.tsx
@@ -2,16 +2,24 @@
 
 import { container } from '@animation/animation';
 import stylesGlobal from '@scss/global.module.scss';
+import ContatoIcons from '@src/components/Contact';
 import useProfile from '@src/hooks/useProfile';
 import { motion } from 'framer-motion';
+import styles from './desktop.module.scss';
 
-export default function ProjectsDesktop() {
+export default function Contact() {
   const { profile } = useProfile();
 
   return (
     <>
       <motion.main variants={container} initial='hidden' animate='visible' className={stylesGlobal.container}>
-        <h1>Desktop Contato</h1>
+        <section className={styles.contact}>
+          <h1>Contate-me</h1>
+          <p>Estou disponível para novos desafios e colaborações. Entre em contato comigo pelas minhas redes sociais ou via WhatsApp 😁😉</p>
+          <section className={styles.icons}>
+            <ContatoIcons />
+          </section>
+        </section>
       </motion.main>
     </>
   );
--- src/app/pages/contact/mobile/index.tsx
+++ src/app/pages/contact/mobile/index.tsx
@@ -2,8 +2,10 @@
 
 import { container } from '@animation/animation';
 import stylesGlobal from '@scss/global.module.scss';
+import ContatoIcons from '@src/components/Contact';
 import useProfile from '@src/hooks/useProfile';
 import { motion } from 'framer-motion';
+import styles from './mobile.module.scss';
 
 export default function Contact() {
   const { profile } = useProfile();
@@ -11,7 +13,11 @@ export default function Contact() {
   return (
     <>
       <motion.main variants={container} initial='hidden' animate='visible' className={stylesGlobal.container}>
-        <h1>Mobile Contato</h1>
+        <section className={styles.contact}>
+          <h1>Contate-me</h1>
+          <p>Estou disponível para novos desafios e colaborações. Entre em contato comigo pelas minhas redes sociais ou via WhatsApp 😁😉</p>
+          <ContatoIcons />
+        </section>
       </motion.main>
     </>
   );
