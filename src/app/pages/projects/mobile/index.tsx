1105b8c15a615be46ceb7f6220ee47c8d92724ad
Douglas Fernandes <dfdsf1996@gmail.com>
2024-09-09 14:33:06 -0300

    Ajuste de tipagem e escrita padronizada.
    - Padronizando as tipagem da className, para melhor semântica no código.

--- src/app/pages/projects/mobile/index.tsx
+++ src/app/pages/projects/mobile/index.tsx
@@ -33,11 +33,11 @@ export default function Projects() {
 
   return (
     <motion.main variants={container} initial='hidden' animate='visible' className={stylesGlobal.container}>
-      <section className={styles.containerCards}>
+      <section className={styles.container__Cards}>
         <h1>Seja bem-vindo</h1>
         <small>Obrigado por visitar meu portfólio! Fico muito feliz em compartilhar meus projetos com você.</small>
 
-        <motion.section layout className={styles.containerProjects}>
+        <motion.section layout className={styles.container__Projects}>
           {projects.map((project, index) => (
             <motion.section
               key={project.titulo}
@@ -45,28 +45,30 @@ export default function Projects() {
               initial={{ borderRadius: 8 }}
               animate={{ borderRadius: expandedProject === index ? 12 : 8 }}
               transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-              className={`${styles.project} ${expandedProject === index ? styles.projectExpanded : ''}`}
+              className={`${styles.project} ${expandedProject === index ? styles.project__Expanded : ''}`}
             >
               {expandedProject !== index && (
-                <header onClick={() => handleCardClick(index)} className={styles.projectHeader}>
+                <header onClick={() => handleCardClick(index)} className={styles.project__Header}>
                   <h2>{project.subtitulo}</h2>
                 </header>
               )}
 
               {expandedProject === index && (
-                <motion.section {...animationProps} className={styles.projectDetails}>
-                  <header onClick={() => handleCardClick(index)} className={styles.projectHeaderExpanded}>
+                <motion.section {...animationProps} className={styles.project__Details}>
+                  <header onClick={() => handleCardClick(index)} className={styles.project__Header_Expanded}>
                     <h2>{project.titulo}</h2>
                   </header>
-                  <motion.button initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5 }} onClick={() => setExpandedProject(null)} className={styles.closeButton}>
+                  <motion.button initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5 }} onClick={() => setExpandedProject(null)} className={styles.close__Button}>
                     X
                   </motion.button>
                   <motion.figure {...animationProps}>
                     <img src={project.imagem} alt={`Imagem do projeto ${project.titulo}`} />
                   </motion.figure>
                   <motion.p {...animationProps}>{project.descricao}</motion.p>
-                  <button type='button' className={styles.verProjetos}>
-                    <a target='_blank' href={project.link}>Click para ver o projeto</a>
+                  <button type='button' className={styles.ver__Projetos}>
+                    <a target='_blank' href={project.link}>
+                      Click para ver o projeto
+                    </a>
                   </button>
                   <motion.ul {...animationProps}>
                     {Object.entries(project.ferramentas).map(([tool, iconUrl]) => (
